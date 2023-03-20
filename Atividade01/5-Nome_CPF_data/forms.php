8080312d16e88837b9d3bbeff17994a55563628e
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-03-20 14:54:42 -0300

    update if/else
    correção do if/else, já q a validação do preenchimento do campo foi feito no formulário html

--- Atividade01/5-Nome_CPF_data/forms.php
+++ Atividade01/5-Nome_CPF_data/forms.php
@@ -4,10 +4,7 @@ $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];;
 $data = $_POST["data"];;
 
-if(empty($nome) || empty($cpf) || empty($data)) {
-    echo "ERRO DE CADASTRO! Verifique os dados e tente novamente.";
-}
-else{
+if(!empty($nome) || !empty($cpf) || !empty($data)) {
     echo "<p>Parabéns $nome !</p>";
     echo "<h4>CADASTRO FEITO COM SUCESSO!</h4";
 }
