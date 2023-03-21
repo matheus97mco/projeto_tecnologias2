744acde59d471c864cfb8bc6a051ba101ee1e7f8
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-03-20 22:50:11 -0300

    Update email.php

--- Atividade01/3-Nome_email_senha/email.php
+++ Atividade01/3-Nome_email_senha/email.php
@@ -4,10 +4,13 @@ $nome = $_POST["nome"];
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-if (!empty($nome) || !empty($email) || !empty($senha)) {
+if (empty($nome) || empty($email) || empty($senha)) {
+    echo "ERRO DE CADASTRO! Verifique os dados e tente novamente.";
+} else {
     echo "<h4>Informações do usuário:</h4>";
     echo "<p>Nome: $nome</p>";
     echo "<p>E-mail: $email</p>";
     echo "<p>Senha: *****</p>";
     echo "<h4>CADASTRO FEITO COM SUCESSO!</h4";
 }
+
