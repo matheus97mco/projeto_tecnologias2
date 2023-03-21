a8c841656f6bcaf42e9fba5b37c6810cee10a288
Matheus Cunha <119085549+matheus97mco@users.noreply.github.com>
2023-03-20 22:51:44 -0300

    Update forms.php

--- Atividade01/5-Nome_CPF_data/forms.php
+++ Atividade01/5-Nome_CPF_data/forms.php
@@ -4,7 +4,10 @@ $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];;
 $data = $_POST["data"];;
 
-if(!empty($nome) || !empty($cpf) || !empty($data)) {
+if(empty($nome) || empty($cpf) || empty($data)) {
+    echo "ERRO DE CADASTRO! Verifique os dados e tente novamente.";
+}
+else{
     echo "<p>Parabéns $nome !</p>";
     echo "<h4>CADASTRO FEITO COM SUCESSO!</h4";
 }
